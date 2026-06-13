072c33d8edf6f8726034d9fede9e69cf4652d9da
John Fletcher <john@bunbury28.plus.com>
2026-06-13 19:20:26 +0100

    Updated files
    Based on OpenCode analysis

--- src/VCANSAM3X8E.cpp
+++ src/VCANSAM3X8E.cpp
@@ -1,30 +1,20 @@
-// Copyright (C) Sven Rosvall (sven@rosvall.ie) (C( John Fletcher (john@bunbury28.plus.com)
+// Copyright (C) Sven Rosvall (sven@rosvall.ie) (C) John Fletcher (john@bunbury28.plus.com)
 // This file is part of VLCB-Arduino project on https://github.com/SvenRosvall/VLCB-Arduino
 // Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 // The full licence can be found at: http://creativecommons.org/licenses/by-nc-sa/4.0/
 
-// Arduino libraries
 #include <SPI.h>
-
-// 3rd party libraries
 #include <Streaming.h>
-
-// VLCB SAM3X8E library
-//#include "CAN2515.h"
 #include "VCANSAM3X8E.h"
-
 
 namespace VLCB
 {
 
-
-//
-/// format and display CAN message
-//
-
-void format_message(CANFrame *msg)
+namespace
 {
 
+void formatMessage(CANFrame *msg)
+{
   char mbuff[80], dbuff[8];
 
   sprintf(mbuff, "[%03ld] [%d] [", (msg->id & 0x7f), msg->len);
@@ -38,79 +28,58 @@ void format_message(CANFrame *msg)
   Serial << "> " << mbuff;
   if (msg->rtr) Serial << " rtr = " << msg->rtr;
   Serial << endl;
-
-  return;
 }
 
-//void something() { }
+}
 
-//
-/// constructor
-//
 VCANSAM3X8E::VCANSAM3X8E()
-  :  _osc_freq(OSCFREQ), _num_rx_buffers(NUM_RX_BUFFS), _num_tx_buffers(NUM_TX_BUFFS), _csPin(MCP2515_CS), _intPin(MCP2515_INT)
 {
-  _instance = 0;
   _can = &Can0;
 }
 
 VCANSAM3X8E::~VCANSAM3X8E()
-{}
-
-//
-/// initialise the CAN controller and buffers, and attach the ISR
-//
+{
+}
 
 bool VCANSAM3X8E::begin(bool /*poll*/, SPIClass /*spi*/)
 {
-
-  uint32_t init_ret;
-  int init_watch;
-
   _numMsgsSent = 0;
   _numMsgsRcvd = 0;
 
-  // init CAN instance
-  init_ret = _can->begin(CAN_BPS_125K, 255);
+  bool init_ret = _can->begin(CAN_BPS_125K, 255);
 
   if (!init_ret) {
-    Serial << "> CAN error from begin(), ret = " << init_ret << endl;
+    if (_debug_on) Serial << "> CAN error from begin(), ret = " << init_ret << endl;
     return false;
   }
 
-  // set filter to permissive
-  init_watch = _can->watchFor();
+  int init_watch = _can->watchFor();
 
-  if (init_watch == -1) {  // Test changed by JPF
-    Serial << "> CAN error from watchFor(), ret = " << init_watch << endl;
+  if (init_watch == -1) {
+    if (_debug_on) Serial << "> CAN error from watchFor(), ret = " << init_watch << endl;
     return false;
   }
 
-  Serial << "> CAN controller instance = " << _instance << " initialised successfully" << endl;
+  if (_debug_on) Serial << "> CAN controller instance = " << _instance << " initialised successfully" << endl;
   return true;
 }
 
 bool VCANSAM3X8E::available(void)
 {
-
   return _can->available();
 }
 
 CANFrame VCANSAM3X8E::getNextCanFrame(void)
 {
-
-  uint32_t ret;
   CAN_FRAME cf;
   CANFrame message;
 
-  ret = _can->read(cf);
+  uint32_t ret = _can->read(cf);
 
-  if (ret != 1 /*CAN_MAILBOX_TRANSFER_OK */) {
-    Serial << "> CAN read did not return CAN_MAILBOX_TRANSFER_OK, instance = " << _instance << ", ret = " << ret << endl;
-    Serial << "> received CAN message has length = " << cf.length << endl;
-  } else {
-    Serial << "> received CAN message ok, instance = " << _instance << ", length = " << cf.length;
-    Serial << ", rtr = " << cf.rtr << endl;
+  if (ret != 1) {
+    if (_debug_on) {
+      Serial << "> CAN read error, instance = " << _instance << ", ret = " << ret << endl;
+    }
   }
 
   message.id = cf.id;
@@ -122,28 +91,23 @@ CANFrame VCANSAM3X8E::getNextCanFrame(void)
 
   ++_numMsgsRcvd;
 
-  format_message(&message);
+  if (_debug_on) {
+    formatMessage(&message);
+  }
   return message;
 }
 
-//
-/// send a CBUS message
-//
-
-bool VCANSAM3X8E::sendCanFrame(CANFrame *msg)  // bool rtr, bool ext, byte priority)
+bool VCANSAM3X8E::sendCanFrame(CANFrame *msg)
 {
-  // note default arguments put here as a fix are not needed.
-  //bool rtr = false; bool ext = false; byte priority = DEFAULT_PRIORITY;
-  Serial << F("CANSAM3X8E sendCanFrame id=") << (msg->id & 0x7F) << " len=" << msg->len << " rtr=" << msg->rtr;
-  if (msg->len > 0)
-    Serial << " op=" << _HEX(msg->data[0]);
-  Serial << endl;
+  if (_debug_on) {
+    Serial << "sendCanFrame id=" << (msg->id & 0x7F) << " len=" << msg->len << " rtr=" << msg->rtr;
+    if (msg->len > 0)
+      Serial << " op=" << _HEX(msg->data[0]);
+    Serial << endl;
+    formatMessage(msg);
+  }
 
-  bool ret;
-  CAN_FRAME cf;  // library-specific CAN message structure
-
-  //makeHeader(msg, priority);          // set the CBUS header - CANID and priority bits
-  format_message(msg);
+  CAN_FRAME cf;
 
   cf.id = msg->id;
   cf.length = msg->len;
@@ -152,74 +116,56 @@ bool VCANSAM3X8E::sendCanFrame(CANFrame *msg)  // bool rtr, bool ext, byte prior
 
   memcpy(cf.data.bytes, msg->data, msg->len);
 
-  ret = _can->sendFrame(cf);
+  uint32_t start = micros();
+  bool ret;
+  do {
+    ret = _can->sendFrame(cf);
+    if (ret) break;
+  } while (micros() - start < 1000);
 
-  if (!ret) {
-    Serial << "> error sending CAN frame, instance = " << _instance << ", ret = " << ret << endl;
+  if (ret) {
+    ++_numMsgsSent;
+  } else {
+    if (_debug_on) {
+      Serial << "> error sending CAN frame, instance = " << _instance << endl;
+    }
   }
-  ++_numMsgsSent;
-
-  // Simple workaround for sending many messages. Let the underlying hardware some time to send this message before next.
-  // TODO: Replace this with monitoring of the transmit queue.
-  delay(1);
 
   return ret;
 }
 
-//
-/// display the CAN bus status instrumentation
-//
-
 void VCANSAM3X8E::printStatus(void)
 {
-
-  return;
+  if (_debug_on) {
+    Serial << "> VLCB status: messages received = " << _numMsgsRcvd
+           << ", sent = " << _numMsgsSent
+           << ", rx errors = " << _can->get_rx_error_cnt()
+           << ", tx errors = " << _can->get_tx_error_cnt()
+           << ", status = " << _can->get_status() << endl;
+  }
 }
-
-//
-/// reset the CAN driver
-//
 
 void VCANSAM3X8E::reset(void)
 {
+  _can->begin(CAN_BPS_125K, 255);
+  _can->watchFor();
+  _numMsgsSent = 0;
+  _numMsgsRcvd = 0;
+
+  if (_debug_on) Serial << "> CAN controller reset" << endl;
 }
-
-//
-/// set the TX and RX pins
-//
-
-
-//void VCANSAM3X8E::setPins(byte /*txPin*/, byte /*rxPin*/)
-//{/
-//
-//
-//return;
-//}
-
-
-//
-/// set the depth of the TX and RX queues
-//
-
-void VCANSAM3X8E::setNumBuffers(byte /*num*/)
-{
-
-  return;
-}
-
-//
-
-//
-/// set the CAN controller peripheral instance, there are two, default is zero
-//
 
 void VCANSAM3X8E::setControllerInstance(byte instance)
 {
-
-  Serial << "> setting CAN controller instance to " << instance << endl;
   _instance = instance;
   _can = (_instance == 0) ? &Can0 : &Can1;
+
+  if (_debug_on) Serial << "> setting CAN controller instance to " << instance << endl;
 }
 
-// End of namespace VLCB
+void VCANSAM3X8E::setDebug(bool new_state)
+{
+  _debug_on = new_state;
+}
+
 }
--- src/VCANSAM3X8E.h
+++ src/VCANSAM3X8E.h
@@ -5,54 +5,29 @@
 
 #pragma once
 
-// header files
-
 #include <Controller.h>
 #include <CanTransport.h>
 #include <SPI.h>
-#include <due_can.h>  // Due CAN library header file
+#include <due_can.h>
 
 namespace VLCB
 {
 
-// constants
-// These are getting defined somewhere else.....
-//static const byte MCP2515_CS = 10;                          // SPI chip select pin
-//static const byte MCP2515_INT = 2;                          // interrupt pin
-//static const byte NUM_RX_BUFFS = 4;                         // default value
-//static const byte NUM_TX_BUFFS = 2;                         // default value
-//static const uint32_t CANBITRATE = 125000UL;                // 125Kb/s - fixed for VLCB
-//static const uint32_t OSCFREQ = 16000000UL;                 // crystal frequency default
-
-//
-/// an implementation of the Transport interface class
-/// to support the SAM3X8E CAN controllers for Arduino DUE.
-//
-
 class VCANSAM3X8E : public CanTransport
 {
 public:
-
   VCANSAM3X8E();
   virtual ~VCANSAM3X8E();
 
-  // these methods are declared virtual in the base class and must be implemented by the derived class
-#ifdef ARDUINO_ARCH_RP2040
-  bool begin(bool poll = false, SPIClassRP2040 spi = SPI);  // note default args
-#else
   bool begin(bool poll = false, SPIClass spi = SPI);
-#endif
   bool available() override;
   CANFrame getNextCanFrame() override;
   bool sendCanFrame(CANFrame *msg) override;
   void reset() override;
 
-  // these methods are specific to this implementation
-  // they are not declared or implemented by the base CBUS class
-  void setNumBuffers(byte num);
-  //void setPins(byte txpin, byte rxPin);
   void printStatus(void);
   void setControllerInstance(byte instance = 0);
+  void setDebug(bool new_state);
 
   virtual unsigned int receiveCounter() override
   {
@@ -75,20 +50,13 @@ public:
     return _can->get_status();
   }
 
-  //ACAN2515 *canp;  // pointer to CAN object so user code can access its members
-
 private:
-  unsigned int _numMsgsSent, _numMsgsRcvd;
-  unsigned long _osc_freq;
-  byte _num_rx_buffers, _num_tx_buffers;
-  byte _csPin, _intPin;
-  bool _poll;
- 
-  byte _instance;
-  CANRaw *_can;
+  unsigned int _numMsgsSent = 0;
+  unsigned int _numMsgsRcvd = 0;
+  byte _instance = 0;
+  CANRaw *_can = nullptr;
+  bool _debug_on = false;
 };
 
-void format_message(CANFrame *msg);
-
-
 }
+
